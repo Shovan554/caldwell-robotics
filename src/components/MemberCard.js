3834a0bd795efd0928d43f1ae4fa01057f370b8a
Narayan Khanal <narayankhanal435@gmail.com>
2024-10-03 01:35:55 -0400

    make a  responsive Team Members page

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,3 +1,5 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
+
 const MemberCard = (props) => {
   return (
     <div className="cardContainer relative w-[30rem] p-4 hover:scale-105 transition-all duration-200 ease-in-out">
@@ -5,6 +7,24 @@ const MemberCard = (props) => {
         <h2 className="name text-2xl font-bold">{props.name}</h2>
         <h4 className="position text-lg font-bold mt-1">{props.position}</h4>
         <p className="mt-[1rem]">{props.information}</p>
+        <div className="socialMediaIcons flex justify-center items-center space-x-4 mt-[2rem]">
+          <a
+            href={props.linkedinLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer"
+          >
+            <FaLinkedin size={25} color="#0e76a8" />
+          </a>
+          <a
+            href={props.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer"
+          >
+            <FaGithub size={25} color="black" />
+          </a>
+        </div>
       </div>
       <div className="profileImage absolute top-[-3rem] left-[37%]">
         <img
@@ -13,7 +33,6 @@ const MemberCard = (props) => {
           alt={`${props.name}`}
         ></img>
       </div>
-      <div className="socialMediaIcon"></div>
     </div>
   );
 };
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,4 +1,5 @@
 import MemberCard from "../components/MemberCard";
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
 
 const Team = () => {
   const executiveMembers = {
@@ -8,6 +9,8 @@ const Team = () => {
       information:
         "Dr. Veksler is an Assistant Professor of Computer Science at Caldwell University. His research and teaching focuses on all topics where there is interplay between humans and machines, including Artificial Intelligence, UI/UX , Human-Computer Interaction, and many more.",
       imgSrc: "./Dr. Veksler.png",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
 
     one: {
@@ -16,6 +19,8 @@ const Team = () => {
       information:
         "Anish Pandey has been the driving force behind the annual Robotics Competition, the development of the Caldwell Makerspace, and the growth of the engineering presence at Caldwell University. His interests span app development, robotics, 3D drafting, ML, and Computer Vision.",
       imgSrc: "./anish.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
     two: {
       name: "Shovan Raut",
@@ -23,6 +28,8 @@ const Team = () => {
       information:
         "Shovan Raut is a Research Assistant at CogAI. Shovan is driven by a desire to innovate and make significant contributions in the field of technology. He looks forward to working on innovative projects, mentoring fellow members, and contributing to advancements in robotics and machine learning.",
       imgSrc: "./shovan.jpg",
+      linkedinLink: "https://www.linkedin.com/in/shovan-raut-81537b18a/",
+      githubLink: "https://github.com/Shovan554",
     },
     three: {
       name: "Saugat Subedi",
@@ -30,6 +37,8 @@ const Team = () => {
       information:
         "Saugat Subedi serves as the Public Relations officer for Caldwell Robotics, holding positions as Chief of Staff in the Student Government Association (SGA) and Student Ambassador for Caldwell Undergraduate Admissions. Passionate about robotics, Subedi acts as bridge between students and administration.",
       imgSrc: "./SAUGAT_SUBEDI.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
     four: {
       name: "Saugat Timilsina",
@@ -37,6 +46,8 @@ const Team = () => {
       information:
         "Saugat is interested in machine learning and robotics, particularly in how these technologies can be applied to solve real-world problems. He is currently focused on expanding his robotics knowledge to other STEM major students at Caldwell University. In addition, he enjoys outdoor games, reading science fiction, and volunteering.",
       imgSrc: "./Saugat Timilsina.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
     five: {
       name: "Dikshya Giri",
@@ -44,6 +55,8 @@ const Team = () => {
       information:
         "Dikshya is a rising sophomore at Caldwell University, majoring in Computer Science. She is the co-founder and serves as the Chief of Logistics and resources for Caldwell Robotics. With the unique fusion of technology and art, she is driven to innovate, contribute and collaborate through Caldwell Robotics, transforming abstract ideas into functional creations.",
       imgSrc: "./Dikshya.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
     six: {
       name: "Dibas Dauliya",
@@ -51,39 +64,47 @@ const Team = () => {
       information:
         "Dibas serves as the Vice President of Marketing and Communications in the Student Government Association (SGA). Additionally, he is an Undergraduate Research Assistant at the icQR/CogAI lab. His unique skills and geniuses has always been a part for the success of every project at Caldwell Robotics.",
       imgSrc: "./Dibas.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
     seven: {
-      name: "Krenjila Sharma",
-      position: "Secretary",
-      information:
-        "Krenjila Sharma is the Lab Manager and Research Assistant for the C-STEM laboratories. With a deep passion for Data Science Krenjila is dedicated to fostering an innovative and collaborative environment. Her commitment to excellence and enthusiasm for cutting-edge research drives her work.",
-      imgSrc: "./krenjila.jpg",
-    },
-    eight: {
-      name: "Yubika Acharya",
-      position: "Treasurer",
-      information:
-        "Yubika serves as the club treasurer for Caldwell Robotics. Majoring in Computer Science and Financial Economics, she combines her passion for robotics with a keen interest in financial management, making this role a perfect fit.",
-      imgSrc: "./Yubika.jpg",
-    },
-    nine: {
       name: "Narayan Khanal",
       position: "IT Specialist",
       information:
         "Narayan Khanal is a junior majoring in Computer Science with a passion for building interactive and responsive websites. As an aspiring web developer and software engineer, he is eager to contribute to Caldwell Robotics, where he can further deepen his knowledge and skills.",
       imgSrc: "./narayan.jpg",
+      linkedinLink: "https://www.linkedin.com/in/narayankhanal/",
+      githubLink: "https://github.com/Nkhanal2002",
+    },
+    eight: {
+      name: "Krenjila Sharma",
+      position: "Secretary",
+      information:
+        "Krenjila Sharma is the Lab Manager and Research Assistant for the C-STEM laboratories. With a deep passion for Data Science Krenjila is dedicated to fostering an innovative and collaborative environment. Her commitment to excellence and enthusiasm for cutting-edge research drives her work.",
+      imgSrc: "./krenjila.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
+    },
+    nine: {
+      name: "Yubika Acharya",
+      position: "Treasurer",
+      information:
+        "Yubika serves as the club treasurer for Caldwell Robotics. Majoring in Computer Science and Financial Economics, she combines her passion for robotics with a keen interest in financial management, making this role a perfect fit.",
+      imgSrc: "./Yubika.jpg",
+      linkedinLink: "https://linkedin.com",
+      githubLink: "https://github.com",
     },
   };
   return (
     <main className="main-container">
-      <div className="sub-header-one h-[40vh] flex flex-col items-center justify-center bg-gradient-to-r from-gray-700 to-slate-700 text-white">
-        <h1 className="sm:text-[3rem] font-extrabold">
+      <div className="sub-header-one h-[30vh] sm:h-[40vh] flex flex-col items-center justify-center bg-gradient-to-r from-gray-700 to-slate-700 text-white">
+        <h1 className="sm:text-[3rem] text-[1.7rem] font-extrabold">
           <span className="text-red-600 mr-4 uppercase">Robotics</span>
           TEAM
         </h1>
       </div>
-      <div className="sub-header-two text-center bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-[2rem]">
-        <h2 className="sm:text-[2rem] font-extrabold uppercase">
+      <div className="sub-header-two text-center bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-[2rem] px-2">
+        <h2 className=" text-xl sm:text-[2rem] font-extrabold uppercase">
           Executive and general Members
         </h2>
       </div>
@@ -93,58 +114,78 @@ const Team = () => {
           position={executiveMembers.one.position}
           information={executiveMembers.one.information}
           imgSrc={executiveMembers.one.imgSrc}
+          linkedinLink={executiveMembers.one.linkedinLink}
+          githubLink={executiveMembers.one.githubLink}
         />
         <MemberCard
           name={executiveMembers.two.name}
           position={executiveMembers.two.position}
           information={executiveMembers.two.information}
           imgSrc={executiveMembers.two.imgSrc}
+          linkedinLink={executiveMembers.two.linkedinLink}
+          githubLink={executiveMembers.two.githubLink}
         />
         <MemberCard
           name={executiveMembers.three.name}
           position={executiveMembers.three.position}
           information={executiveMembers.three.information}
           imgSrc={executiveMembers.three.imgSrc}
+          linkedinLink={executiveMembers.three.linkedinLink}
+          githubLink={executiveMembers.three.githubLink}
         />
         <MemberCard
           name={executiveMembers.four.name}
           position={executiveMembers.four.position}
           information={executiveMembers.four.information}
           imgSrc={executiveMembers.four.imgSrc}
+          linkedinLink={executiveMembers.four.linkedinLink}
+          githubLink={executiveMembers.four.githubLink}
         />
         <MemberCard
           name={executiveMembers.five.name}
           position={executiveMembers.five.position}
           information={executiveMembers.five.information}
           imgSrc={executiveMembers.five.imgSrc}
+          linkedinLink={executiveMembers.five.linkedinLink}
+          githubLink={executiveMembers.five.githubLink}
         />
         <MemberCard
           name={executiveMembers.six.name}
           position={executiveMembers.six.position}
           information={executiveMembers.six.information}
           imgSrc={executiveMembers.six.imgSrc}
+          linkedinLink={executiveMembers.six.linkedinLink}
+          githubLink={executiveMembers.six.githubLink}
         />
         <MemberCard
           name={executiveMembers.seven.name}
           position={executiveMembers.seven.position}
           information={executiveMembers.seven.information}
           imgSrc={executiveMembers.seven.imgSrc}
+          linkedinLink={executiveMembers.seven.linkedinLink}
+          githubLink={executiveMembers.seven.githubLink}
         />
         <MemberCard
           name={executiveMembers.eight.name}
           position={executiveMembers.eight.position}
           information={executiveMembers.eight.information}
           imgSrc={executiveMembers.eight.imgSrc}
+          linkedinLink={executiveMembers.eight.linkedinLink}
+          githubLink={executiveMembers.eight.githubLink}
         />
         <MemberCard
           name={executiveMembers.nine.name}
           position={executiveMembers.nine.position}
           information={executiveMembers.nine.information}
           imgSrc={executiveMembers.nine.imgSrc}
+          linkedinLink={executiveMembers.nine.linkedinLink}
+          githubLink={executiveMembers.nine.githubLink}
         />
       </div>
       <div className="sub-header-three text-center bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-[1rem]">
-        <h2 className="sm:text-[2rem] font-extrabold uppercase">Our Advisor</h2>
+        <h2 className=" text-xl  sm:text-[2rem] font-extrabold uppercase">
+          Our Advisor
+        </h2>
       </div>
       <div className="advisor-container flex justify-center items-center mt-[5rem]">
         <MemberCard
@@ -152,14 +193,16 @@ const Team = () => {
           position={executiveMembers.advisor.position}
           information={executiveMembers.advisor.information}
           imgSrc={executiveMembers.advisor.imgSrc}
+          linkedinLink={executiveMembers.advisor.linkedinLink}
+          githubLink={executiveMembers.advisor.githubLink}
         />
       </div>
       <div className="sub-header-four text-center bg-gradient-to-r from-violet-600 to-indigo-600 text-white py-[1rem]">
-        <h2 className="sm:text-[2rem] font-extrabold uppercase">
+        <h2 className="text-xl  sm:text-[2rem] font-extrabold uppercase">
           Website Developers
         </h2>
       </div>
-      <div className="website-developers sm:flex sm:justify-between sm:items-center mt-10 w-[50rem] mx-auto p-4">
+      <div className="website-developers flex justify-center items-center mx-auto space-x-10 sm:flex sm:justify-center sm:items-center mt-10 w-[50%] sm:space-x-[5rem] sm:mx-auto p-4">
         <section className="flex flex-col justify-center items-center">
           <img
             src="./shovan.jpg"
@@ -167,6 +210,28 @@ const Team = () => {
             className="w-[7rem] rounded-full "
           ></img>
           <h2 className="sm:text-xl font-bold  mt-1">Shovan Raut</h2>
+          <div className="socialIcons flex justify-center space-x-3 mt-2">
+            <a
+              href="https://www.linkedin.com/in/shovan-raut-81537b18a/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <div className="w-[20px] sm:w-[25px]">
+                <FaLinkedin className=" h-full w-full" color="#0e76a8" />
+              </div>
+            </a>
+            <a
+              href="https://github.com/Shovan554"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <div className="w-[20px] sm:w-[25px]">
+                <FaGithub className=" h-full w-full" color="black" />
+              </div>
+            </a>
+          </div>
         </section>
         <section className="flex flex-col justify-center items-center">
           <img
@@ -175,14 +240,28 @@ const Team = () => {
             className="w-[7rem] rounded-full"
           ></img>
           <h2 className="sm:text-xl font-bold mt-1">Narayan Khanal</h2>
-        </section>
-        <section className="flex flex-col justify-center items-center">
-          <img
-            src="./Dikshya.jpg"
-            alt="dikshya"
-            className="w-[7rem] rounded-full"
-          ></img>
-          <h2 className="sm:text-xl font-bold mt-1">Dikshya Giri</h2>
+          <div className="socialIcons flex justify-center space-x-3 mt-2">
+            <a
+              href="https://www.linkedin.com/in/narayankhanal/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <div className="w-[20px] sm:w-[25px]">
+                <FaLinkedin className=" h-full w-full" color="#0e76a8" />
+              </div>
+            </a>
+            <a
+              href="https://github.com/Nkhanal2002"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer"
+            >
+              <div className="w-[20px] sm:w-[25px]">
+                <FaGithub className=" h-full w-full" color="black" />
+              </div>
+            </a>
+          </div>
         </section>
       </div>
     </main>
