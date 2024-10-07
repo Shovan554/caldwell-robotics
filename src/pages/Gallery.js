083709f007b845bd95e8f9ca17cbda2476ecccda
Shovan Raut <shovan@shovans-mbp.myfiosgateway.com>
2024-10-07 10:01:03 -0400

    added gallery page

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,16 +4,7 @@ import '../styles/gallery.css'; // Import the Gallery CSS
 
 // Array of images with src and width/height attributes
 const photos = [
-  {
-    src: require('../assets/gallery/robosoccer/anish.jpg').default, // Match the exact file name
-    width: 4,
-    height: 3,
-  },
-  {
-    src: require('../assets/gallery/robosoccer/Dibas.jpg').default, // Match the exact file name
-    width: 4,
-    height: 3,
-  },
+  
 ];
 
 const GalleryPage = () => {
